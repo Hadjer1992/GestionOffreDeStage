399d6017f0643e70a38db044b29489a90b61ee4a
Hadjer1992 <hadjer.sahraoui.lahreche@gmail.com>
2017-04-24 14:13:21 +0200

    projet
    organiser et complet le projet

---
+++ Projet/src/Metier/implementsEntreprise.java
@@ -0,0 +1,13 @@
+package Metier;
+
+
+import java.util.ArrayList;
+
+import javafx.stage.Stage;
+
+public interface implementsEntreprise{
+	
+	public void deposerOffre(Stage s);
+	public void supprimerOffre(int id);
+	public void search(ArrayList<String> list);
+}
